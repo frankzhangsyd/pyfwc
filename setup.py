2ce31d570fb5c918216590b8074b48d401ad4727
frankzhangsyd <frank.dingrui@gmail.com>
2022-04-21 17:54:44 +1000

    Update it to version 0.2.0

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/frankzhangsyd/pyfwc'
 EMAIL = 'frank@dingrui.com'
 AUTHOR = 'Frank Zhang'
 REQUIRES_PYTHON = '>=3.7.4'
-VERSION = '0.1.0'
+VERSION = '0.2.0'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
